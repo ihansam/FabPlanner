c6ace0c57b8a7e47dd998dbff77b05968212f8df
ihansam <ihansam0322@gmail.com>
2023-08-14 15:38:26 +0900

    implement #6

--- front/pages/01_Project.py
+++ front/pages/01_Project.py
@@ -6,4 +6,9 @@ print("run project page")
 st.markdown("# Projects  ")
 
 _projects = get_all_project()
-st.dataframe(_projects)
+print(_projects)
+
+_filter = st.text_input("Find project by codename", "")
+projects = _projects[_projects['codeName'].str.contains(_filter, case=False)]
+
+st.dataframe(projects, hide_index=True)
